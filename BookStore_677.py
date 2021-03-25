456d04d0df88308a31bab38f58bf572e01ce4d62
William Burton <wdburton@umass.edu>
2021-03-25 01:47:50 -0400

    Basic comments in makeDocs written

--- BookStore_677.py
+++ BookStore_677.py
@@ -33,7 +33,30 @@ def printCat(toPrint):
 #   Catalogue
 def cat_makeDocs(runNum = -1, id_1 = 2, id_2 = 2, id_3 = 3, id_4 = 2):
     pass
-    # This function creates the catalogue logs; First, 
+    # This function creates the catalogue logs; First, it checks to see if the correct folder
+    # exists.  If not, it creates it.  Then, it creates a new CATALOGUE.txt, using 'w' mode
+    # for python file io; it fills the file and closes it.  Finally, if the run number is defined,
+    # it creates the correct document, ORDER_LOGXX.txt, in the same manner and fills the first line.
+    # Otherwise, it finds the first clear run number and uses that.  Either way, it returns the run
+    # number used.
+
+    # Create the folder if it doesn't exists
+
+    # Create CATALOGUE.txt, and fill it
+
+    # Close CATALOGUE.txt file
+
+    # Variable for the true run number
+
+    # Check to see if the run number is defined!
+
+        # If defined, store in true run number
+
+        # Otherwise, search for first free doc name, and use that for the run number
+
+    # Now that run number is defined, make the file and put in the first line
+
+    # Close ORDER_LOGXX.txt and return the run number.
 
 
 
